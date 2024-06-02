753366ff066de087749e3d1f4b39c7483059196a
SheilaV7 <145947567+SheilaV7@users.noreply.github.com>
2024-06-01 19:37:02 -0500

    Update package.scala

--- src/main/scala/Itinerarios/package.scala
+++ src/main/scala/Itinerarios/package.scala
@@ -119,36 +119,7 @@ package object Itinerarios {
   }
 
 
-  def itinerariosAire(vuelos:List[Vuelo], aeropuertos:List[Aeropuerto]):(String, String) => List[Itinerario] = {
-        //Obtiene todos los itinerarios podibles
-        val funcionItinerario = itinerarios(vuelos, aeropuertos)
-        //Calcula el tiempo total de un solo vuelo
-        def funTiempo(vuelo:Vuelo):Double = {
-            val a1 = aeropuertos.filter(_.Cod == vuelo.Org)
-            val a2 = aeropuertos.filter(_.Cod == vuelo.Dst)
-            val gmtSalida = (a1.head).GMT/100
-            val gmtLlegada = (a2.head).GMT/100
-            val horaSalida = (vuelo.HS + (vuelo.MS.toDouble/60)) - gmtSalida
-            val horaLlegada = (vuelo.HL + (vuelo.ML.toDouble/60)) - gmtLlegada
-            val tiempoVuelo = horaLlegada - horaSalida
-            if (tiempoVuelo <= 0) 24 + tiempoVuelo else tiempoVuelo
-        }
-        //Calcula el tiempo total de vuelo de un itinerario
-        def calcularTiempo (v:Itinerario):Double = {
-           (v map (i => funTiempo(i))).sum
-        }
-        //Funcion de salida, calcula los tres itinerarios que tienen menor tiempo en el aire
-        def miItinerario (aeropuerto1:String, aeropuerto2:String): List[Itinerario] = {
-            val misItinerarios = funcionItinerario(aeropuerto1, aeropuerto2)
-            if (misItinerarios.length <= 3) misItinerarios 
-            else {
-                val tiempos = misItinerarios map (c => (c, calcularTiempo(c)))
-                val salida = tiempos.sortBy(_._2)
-                (((salida.unzip)._1).toList).take(3)
-            }
-        }
-        miItinerario
-    }
+  
   
   def itinerariosEscalas(vuelos: List[Vuelo], aeropuertos: List[Aeropuerto]): (String, String) => List[Itinerario] = {
     //guarda los valores de los itinerarios 
